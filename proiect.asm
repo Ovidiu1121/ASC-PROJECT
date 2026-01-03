c528276d1111855cc29a1f853ac24c466cf60257
denisac159 <paula.costin@stud.ubbcluj.ro>
2026-01-03 20:32:56 +0200

    mesaj

--- proiect.asm
+++ proiect.asm
@@ -7,12 +7,20 @@ data SEGMENT
     sir     db 16 dup(?)
     len     db 0
     C       dw ?
+    msgInput db 'Introduceti octetii in format hex: $'
+    msgSorted db 0Dh, 0Ah, 'Sirul sortat: $'  ;0Dh, 0Ah = enter (linie noua)
+    msgC      db 0Dh,0Ah,'Cuvantul C calculat: $'
+    msgRotate db 0Dh,0Ah,'Sirul dupa rotiri: $'
 data ENDS
 
 code SEGMENT
 start:
     mov ax, data
     mov ds, ax
+    
+    lea dx, msgInput ;mesaj
+    call PrintString
+
     mov ah, 0Ah
     lea dx, inbuf
     int 21h
@@ -85,7 +93,9 @@ DoneParse:
 SumLoop:
     add al, [si]
     inc si
-    loop SumLoop
+    ;loop SumLoop
+    dec cx
+    jnz SumLoop
     mov ah, al
 
     lea si, sir
@@ -112,7 +122,9 @@ OrLoop:
     and al, 0Fh
     or  bh, al
     inc si
-    loop OrLoop
+    ;loop OrLoop
+    dec cx
+    jnz OrLoop
 
     mov al, bh
     shl al, 4
@@ -140,6 +152,10 @@ NoSwap:
     jnz InnerSort
     dec cl
     jnz OuterSort
+
+    lea dx, msgSorted ;mesaj
+    call PrintString
+
     lea si, sir
     mov cl, len
     xor ch, ch
@@ -164,11 +180,78 @@ CheckNext:
 SkipElem:
     inc si
     inc bl
-    loop CheckNext
+    dec cx
+    jnz CheckNext
+    ;loop CheckNext
 
     inc dl
     xor dh, dh
     mov C, dx
+    
+    lea dx, msgC ;mesaj
+    call PrintString
+
+    mov al, byte ptr C+1 ;byte superior
+    call PrintHexByte
+    mov al, byte ptr C   ;byte inferior
+    call PrintHexByte  ;afisam C in hex
+	
+    mov dl, ' ' ;spatiu intre hex si binar
+    call PrintChar
+
+    mov al, byte ptr C+1  ;byte superior
+    call PrintBinaryByte
+    mov al, byte ptr C    ;byte inferior
+    call PrintBinaryByte
+
+;rotirea fiecarui octet:
+    lea si, sir ;si=inceputul sirului de octeti
+    mov cl, len ;cl=nr de octeti
+    xor ch,ch   ;cx=contor pentru bucla
+
+RotateLoop: 
+    mov al, [si] ;octetul curent din sir
+
+    ;calcul N=suma primilor 2 biti
+    mov bl, al ;copiem octetul in bl
+    and bl, 00000011b ;pastram doar bitul 0 si bitul 1, BL=N
+   
+    push cx  ;salvam contorul
+    mov cl, bl
+    rol al, cl     ;rotire circulara la stanga cu N pozitii
+    pop cx    ;refacem contorul
+
+    mov [si], al      ;salvam rezultatul 
+    inc si   ;trecem la urmatorul octet 
+    dec cx
+    jnz RotateLoop 
+lea dx, msgRotate
+call PrintString ;mesaj
+
+;afisarea octetilor (pentru fiecare se afiseaza hex si bina/r)
+lea si, sir
+mov cl, len
+xor ch, ch 
+
+ShowLoop:
+    mov al, [si]  ;al=octetul curent
+
+    call PrintHexByte ;afiseaza octetul din AL in hex
+    mov dl, ' '       ;spatiu intre hex si binar
+    call PrintChar    
+
+    mov al, [si]   ;reincarcam octetul
+    call PrintBinaryByte ;afisarea in binar
+
+    mov dl, 0Dh
+    call PrintChar
+    mov dl, 0Ah
+    call PrintChar ;trecem la linie noua
+
+    inc si  ;trecem la urmatorul octet de afisat
+    dec cx 
+    jnz ShowLoop
+
 
 Finish:
     mov ax, 4C00h
@@ -218,15 +301,88 @@ CountBits PROC
     mov cl, 8
     xor ah, ah
 
-BitLoop:
+CountBitLoop:
     shr al, 1
     adc ah, 0
-    loop BitLoop
+    loop CountBitLoop
 
     mov al, ah
     pop cx
     ret
 CountBits ENDP
 
+PrintChar PROC ;afisarea unui caracter ASCII 
+    mov ah, 02h
+    int 21h
+    ret
+PrintChar ENDP
+
+PrintHexByte PROC  ;afisarea unui octet in hex
+   push ax ;salvam AX
+   push dx ;salvam DX
+
+   mov ah, al ;mutam octetul in ah
+   shr ah, 4 ;deplasam la dreapta 4 pozitii (mutam nibble-ul superior jos)
+   mov al, ah  ;AL=nibble
+   call NibbleToHex ;convertim 
+   mov dl, al
+   call PrintChar ;afisam caracterul hex
+
+   pop dx
+   pop ax ;refacem registrele
+
+   and al, 0Fh ;pastram nibble ul inferior 
+   call NibbleToHex
+
+   mov dl, al
+   call PrintChar ;afisam al doilea caracter hex
+
+   ret
+PrintHexByte ENDP
+
+NibbleToHex PROC ;conversie nibble -> caracter
+   cmp al, 9
+   jbe Digit
+
+   add al, 'A' - 10
+   ret
+
+Digit:
+   add al, '0'
+   ret
+NibbleToHex ENDP
+
+PrintBinaryByte PROC ;afisarea unui octet in binar
+   push ax
+   push cx
+   push dx
+
+   mov cx, 8 ;vom afisa 8 biti
+
+BitLoop:
+   shl al, 1 ;bitul MSB ajunge in CF
+   jc BitOne ;daca CF=1
+
+   mov dl, '0'
+   jmp PrintBit
+
+BitOne:
+   mov dl, '1'
+
+PrintBit:
+   call PrintChar  ;afisam bitul
+   loop BitLoop
+
+   pop dx
+   pop cx
+   pop ax
+   ret
+PrintBinaryByte ENDP
+
+PrintString PROC ;afisarea unui mesaj
+   mov ah, 09h
+   int 21h
+   ret
+PrintString ENDP
 code ENDS
 END start
